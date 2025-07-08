98ce1f3c1d5bf6ee6d1af0a0ea5b9c02ead46ed1
Daniel V. Santamaria <daneduvsant@gmail.com>
2025-07-07 22:40:35 -0400

    renombrado index a pruebaAccesoPagina.js

--- pruebaAccesoPagina.js
+++ pruebaAccesoPagina.js
@@ -1,7 +1,9 @@
 const { chromium } = require('playwright');
 
+// Esta función prueba si hay acceso a la página del BCV y toma una captura de pantalla
+// para verificar que se puede acceder correctamente.
 ;(async () => {
-    const browser = await chromium.launch({ headless: false }) // Launch browser in non-headless mode
+    const browser = await chromium.launch({ headless: false }) //
     const page = await browser.newPage()
     await page.goto('https://www.bcv.org.ve/')
     await page.screenshot({ path: 'bcvTest.png' })
